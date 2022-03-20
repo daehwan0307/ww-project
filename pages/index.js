cca353b16f8239ccec0a5752ed34f4ffc3dd02a5
daehwan0307 <eoghks960307@naver.com>
2022-03-20 23:07:52 +0900

    index page

--- pages/index.js
+++ pages/index.js
@@ -7,11 +7,22 @@ export default function Home() {
   return (
     <div className='mt-16'>
       <div className='flex flex-col items-center items-center'>
-      <Image   src="/favicon.ico" width={200} height={300}/>
+      <Image src="/favicon.ico" width={200} height={300}/>
       </div>
       
       <div className='flex mt-8 flex-col items-center'>
       <KakaoLoginButton/>
+      </div>
+
+      <div className='flex flex-col  mt-4 '>
+        <div className='grid grid-cols-2 w-full'>
+          <button>
+            이메일로 회원가입
+          </button>
+          <button>
+            이메일로 로그인
+          </button>
+        </div>
       </div>
     
     </div>
