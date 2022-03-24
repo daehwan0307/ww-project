629c7d88d4cd3a01ee485adced5b0ec5ec7c158b
daehwan0307 <eoghks960307@naver.com>
2022-03-24 14:44:44 +0900

    client

---
+++ pages/api/client-test.js
@@ -0,0 +1,13 @@
+import client from "../../libs/client";
+
+export default async function handler(req, res) {
+  await client.user.create({
+    data: {
+      email: "hi",
+      name: "hi",
+    },
+  });
+  res.json({
+    ok: true,
+  });
+}
