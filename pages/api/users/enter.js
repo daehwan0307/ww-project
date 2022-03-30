fc17ec8c1cb580d233457a4afd1f42a5c135edea
daehwan0307 <eoghks960307@naver.com>
2022-03-30 14:12:02 +0900

    token setting

--- pages/api/users/enter.js
+++ pages/api/users/enter.js
@@ -4,18 +4,25 @@ import withHandler from "../../../libs/server/withHandler";
 
 async function handler(req, res) {
   const { phone, email } = req.body;
-  const payload = phone ? { phone: +phone } : { email };
-  const user = await client.user.upsert({
-    where: {
-      ...payload,
+  const user = phone ? { phone: +phone } : { email };
+  const payload = Math.floor(100000 + Math.random() * 900000) + "";
+  const token = await client.token.create({
+    data: {
+      payload,
+      user: {
+        connectOrCreate: {
+          where: {
+            ...user,
+          },
+          create: {
+            name: "Anonymous",
+            ...user,
+          },
+        },
+      },
     },
-    create: {
-      name: "Anonymous",
-      ...payload,
-    },
-    update: {},
   });
-  console.log(user);
+  console.log(token);
   /* if (email) {
     user = await client.user.findUnique({
       where: {
@@ -54,5 +61,4 @@ async function handler(req, res) {
   } */
   return res.status(200).end();
 }
-
 export default withHandler("POST", handler);
