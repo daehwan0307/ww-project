d03c8a06db5ba32610dbddc60fb7cb23f0ca587a
daehwan0307 <eoghks960307@naver.com>
2022-03-31 15:42:05 +0900

    iron-session 설치

--- pages/api/users/confirm.js
+++ pages/api/users/confirm.js
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import withHandler from "../../../libs/server/withHandler";
 import client from "../../../libs/server/client";
+import {withIronSessionApiRoute} from 
 
 async function handler(req, res) {
   const { token } = req.body;
