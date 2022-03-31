ea3cd642a06c94fae9d674625733369c08efd2a5
daehwan0307 <eoghks960307@naver.com>
2022-03-31 16:09:41 +0900

    session

--- pages/api/users/confirm.js
+++ pages/api/users/confirm.js
@@ -1,12 +1,25 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import withHandler from "../../../libs/server/withHandler";
 import client from "../../../libs/server/client";
-import {withIronSessionApiRoute} from 
+import { withIronSessionApiRoute } from "iron-session/next";
 
 async function handler(req, res) {
   const { token } = req.body;
-  console.log(token);
+  const exists = await client.token.findUnique({
+    where: {
+      payload: token,
+    },
+  });
+  if (!exists) res.status(404).end();
+  req.session.user = {
+    id: exists?.userId,
+  };
+  await req.session.save();
   res.status(200).end();
 }
 
-export default withHandler("POST", handler);
+export default withIronSessionApiRoute(withHandler("POST", handler), {
+  cookieName: "winwinsession",
+  password:
+    "9845904809485098594385093840598dfasdsadasdasdasdas;ldfksjgdsflgjdfklgjdflgjflkgjdgd",
+});
--- pages/enter.js
+++ pages/enter.js
@@ -28,6 +28,7 @@ const Enter = () => {
     if (tokenLoading) return;
     confirmToken(validForm);
   };
+  console.log(loading);
   return (
     <div className="mt-16 px-4">
       <h3 className="text-3xl font-bold text-center">Enter to WinWin</h3>
