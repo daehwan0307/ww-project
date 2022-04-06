71f23a2318feba77fc4b7cedf6ae0793c5f9ca9c
daehwan0307 <eoghks960307@naver.com>
2022-04-06 15:24:32 +0900

    post detail

--- pages/community/write.js
+++ pages/community/write.js
@@ -2,13 +2,34 @@ import { NextPage } from "next";
 import Button from "../../components/button";
 import Layout from "../../components/layout";
 import TextArea from "../../components/textarea";
+import { useForm } from "react-hook-form";
+import useMutation from "../../libs/client/useMutation";
+import { useEffect } from "react";
+import { Post } from "@prisma/client";
+import { useRouter } from "next/router";
 
 const Write = () => {
+  const router = useRouter();
+  const { register, handleSubmit } = useForm();
+  const [post, { loading, data }] = useMutation("/api/posts");
+  const onValid = (data) => {
+    if (loading) return;
+    post(data);
+  };
+  useEffect(() => {
+    if (data && data.ok) {
+      router.push(`/community/${data.post.id}`);
+    }
+  }, [data, router]);
   return (
     <Layout canGoBack title="Write Post">
-      <form className="p-4 space-y-4">
-        <TextArea required placeholder="Ask a question!" />
-        <Button text="Submit" />
+      <form onSubmit={handleSubmit(onValid)} className="p-4 space-y-4">
+        <TextArea
+          register={register("question", { required: true, minLength: 5 })}
+          required
+          placeholder="Ask a question!"
+        />
+        <Button text={loading ? "Loading..." : "Submit"} />
       </form>
     </Layout>
   );
