2009ba606b39a6e6682e173d6c5950cbfeb5f117
daehwan0307 <eoghks960307@naver.com>
2022-04-07 17:42:17 +0900

    profile 수정

--- libs/client/useUser.js
+++ libs/client/useUser.js
@@ -1,6 +1,7 @@
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 import useSWR from "swr";
+import { User } from "@prisma/client";
 
 export default function useUser() {
   const { data, error } = useSWR("/api/users/me");
