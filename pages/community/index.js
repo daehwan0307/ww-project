b8d6f26cc648907a729060182f147046dab4ba90
daehwan0307 <eoghks960307@naver.com>
2022-04-14 19:24:00 +0900

    수정

--- pages/community/index.js
+++ pages/community/index.js
@@ -14,7 +14,7 @@ const Community = () => {
       : null
   );
   return (
-    <Layout hasTabBar title="동네생활">
+    <Layout hasTabBar title="동네생활" Home>
       <div className="space-y-4 divide-y-[2px]">
         {data?.posts?.map((post) => (
           <Link key={post.id} href={`/community/${post.id}`}>
