1dddf6c6b05a6f6a35ead1c8a32ca6ddecb6a472
daehwan0307 <eoghks960307@naver.com>
2022-04-20 15:06:56 +0900

    middleware 설정

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -2,13 +2,11 @@ import { NextRequest, NextFetchEvent } from "next/server";
 import { NextResponse } from "next/server";
 
 export function middleware(req, ev) {
-  if (req.ua?.isBot) {
-    return new Response("Plz don't be a bot. Be human.", { status: 403 });
-  }
   if (!req.url.includes("/api")) {
-    if (!req.url.includes("/enter") && !req.cookies.carrotsession) {
-      return NextResponse.redirect("/enter");
+    if (!req.url.includes("/enter") && !req.cookies.winwinsession) {
+      const url = req.nextUrl.clone();
+      url.pathname = "/enter";
+      return NextResponse.redirect(url);
     }
   }
-  //  return NextResponse.json({ ok: true });
 }
